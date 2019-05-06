8ae005b03866ec3eeca9ad3b4f4df49368c66e0a
CrossroadsMan <crossroadsman@gmail.com>
2019-05-06 16:47:36 -0600

    Create basic view to show the uploaded image

--- cropper/views.py
+++ cropper/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from PIL import Image
+
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 
+@ensure_csrf_cookie
 def cropper(request):
     template = 'cropper/cropper.html'
     context = {}
     return render(request, template, context)
+
+
+def upload_image(request):
+
+    image_file = request.FILES.get('image')
+
+    if image_file:
+        # unedited image is passed in as a file attached to the form
+        print("Image is in request.FILES")
+
+        image_object = Image.open(image_file)
+        image_object.show()
+
+    else:
+        print("no image file")
+    
+    
+    return redirect('cropper:cropper')
