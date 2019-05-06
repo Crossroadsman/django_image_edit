99bcbe76f5088bfbcc9640c02e9919521b47391b
CrossroadsMan <crossroadsman@gmail.com>
2019-05-06 16:44:53 -0600

    Create new url pattern for the upload image action to POST to

--- cropper/urls.py
+++ cropper/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = 'cropper'
 urlpatterns = [
     re_path(r'^$', views.cropper, name='cropper'),
+    re_path(r'upload_image$', views.upload_image, name='upload_image'),
     #re_path(r'^(?P<id>\d+)$', views.cropper, name='cropper'),
     #re_path(r'^(?P<id>\d+)/process$', views.process, name='process'),
 ]
